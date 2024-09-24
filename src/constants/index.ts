0696923db2b49ba01c72e81ee1fc2f2c6cbd3d71
Razmik <razmik@Razmiks-MacBook-Pro.local>
2024-09-25 03:07:58 +0400

    feat: added appropriate env files

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.REACT_APP_BASE_URL;
+export const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 export const APP_PATHS = {
   createCooperation: "/cooperation/create/",
